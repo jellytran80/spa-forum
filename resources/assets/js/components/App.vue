0ca867a5c5b5aa9cd0274be80ffdd573367b9ca1
anhtt <anhtranthe4788@gmail.com>
2017-10-16 17:27:15 +0000

    Setting Vue

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+	<div id="root">
+		<h1>
+			SPA-FORUM Vue2 & Laravel
+		</h1>
+		<router-view></router-view>
+	</div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,16 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+<html lang="vi VN">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>FORUMS</title>
-        <!-- Main styles for this application -->
+        <title>SPA Forum</title>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <meta id="csrf-token" name="csrf-token" value="{{ csrf_token() }}">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div id="app">
+                <example></example>
         </div>
+
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
