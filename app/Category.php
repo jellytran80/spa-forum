955160a1264fec9439c08a38bd04afe44a821d9d
anhtt <anhtranthe4788@gmail.com>
2017-10-23 17:02:18 +0000

    Add API categories and topics by category

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
-    public function topics(){
-    	return $this->hasMany(Topic::class);
+	// JSON format
+    protected $visible = ['id', 'name', 'description', 'numberOfTopics','topics'];
+    //  numberOfTopics chứa số lượng chủ đề trong danh mục
+    protected $appends = ['numberOfTopics'];
+    // Một danh mục có nhiều topics
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+    // Đếm số topics của 1 danh mục
+    public function getNumberOfTopicsAttribute()
+    {
+        return $this->topics->count();
     }
 }
--- app/Topic.php
+++ app/Topic.php
@@ -6,18 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
-    public function category(){
-    	return $this->belongsTo(Category::class);
+	// Các trường sẽ hiển thị trong json
+    protected $visible = ['id', 'title', 'body', 'views', 'time', 'category_id'];
+    // Thời gian dùng để hiển thị
+    protected $appends = ['time'];
+
+    public function getTimeAttribute()
+    {
+        return $this->created_at->timestamp;
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
     public function comments()
-	{
-	    return $this->hasMany(Comment::class);
-	}
-
-	public function user()
-	{
-	    return $this->belongsTo(User::class);
-	}
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
